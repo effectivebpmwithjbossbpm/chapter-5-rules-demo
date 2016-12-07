23454b8c6c1c8f7fc3b8adbc47204f6ca8446022
Eric D. Schabell <eric@schabell.org>
2016-12-07 14:48:24 +0100

    Updated to BPM Suite 6.4 and EAP 7.0, tagging version 1.1.

--- init.bat
+++ init.bat
@@ -6,17 +6,16 @@ set DEMO=Chapter 5 Rules Demo
 set AUTHORS=Andrew Block, Eric D. Schabell
 set PROJECT=git@github.com:effectivebpmwithjbossbpm/chapter-5-rules-demo.git
 set PRODUCT=JBoss BPM Suite
-set JBOSS_HOME=%PROJECT_HOME%target\jboss-eap-6.4
+set JBOSS_HOME=%PROJECT_HOME%target\jboss-eap-7.0
 set SERVER_DIR=%JBOSS_HOME%\standalone\deployments
 set SERVER_CONF=%JBOSS_HOME%\standalone\configuration\
 set SERVER_BIN=%JBOSS_HOME%\bin
 set SRC_DIR=%PROJECT_HOME%\installs
 set SUPPORT_DIR=%PROJECT_HOME%\support
 set PRJ_DIR=%PROJECT_HOME%\projects
-set BPMS=jboss-bpmsuite-6.3.0.GA-installer.jar
-set EAP=jboss-eap-6.4.0-installer.jar
-set EAP_PATCH=jboss-eap-6.4.7-patch.zip
-set VERSION=6.3
+set BPMS=jboss-bpmsuite-6.4.0.GA-deployable-eap7.x.zip
+set EAP=jboss-eap-7.0.0-installer.jar
+set VERSION=6.4
 
 REM wipe screen.
 cls
@@ -47,17 +46,7 @@ if exist "%SRC_DIR%\%EAP%" (
         echo Product sources are present...
         echo.
 ) else (
-        echo Need to download %EAP% package from the Customer Support Portal
-        echo and place it in the %SRC_DIR% directory to proceed...
-        echo.
-        GOTO :EOF
-)
-
-if exist "%SRC_DIR%\%EAP_PATCH%" (
-        echo Product patches are present...
-        echo.
-) else (
-        echo Need to download %EAP_PATCH% package from the Customer Support Portal
+        echo Need to download %EAP% package from http://developers.redhat.com
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
@@ -67,7 +56,7 @@ if exist "%SRC_DIR%\%BPMS%" (
         echo Product sources are present...
         echo.
 ) else (
-        echo Need to download %BPMS% package from the Customer Support Portal
+        echo Need to download %BPMS% package from http://developers.redhat.com
         echo and place it in the %SRC_DIR% directory to proceed...
         echo.
         GOTO :EOF
@@ -80,7 +69,6 @@ if exist %JBOSS_HOME% (
         
          rmdir /s /q "%JBOSS_HOME%"
  )
-
 
 REM Run installers.
 echo EAP installer running now...
@@ -95,24 +83,10 @@ if not "%ERRORLEVEL%" == "0" (
 	GOTO :EOF
 )
 
-call set NOPAUSE=true
-
-echo.
-echo Applying JBoss EAP patch now...
-echo.
-call "%JBOSS_HOME%\bin\jboss-cli.bat" --command="patch apply %SRC_DIR%\%EAP_PATCH% --override-all"
-
-if not "%ERRORLEVEL%" == "0" (
-  echo.
-	echo Error Occurred During JBoss EAP Patch Installation!
-	echo.
-	GOTO :EOF
-)
-
 echo.
 echo BPM Suite installer running now...
 echo.
-call java -jar "%SRC_DIR%\%BPMS%" "%SUPPORT_DIR%\installation-bpms" -variablefile "%SUPPORT_DIR%\installation-bpms.variables"
+cscript /nologo %SUPPORT_DIR%\unzip.vbs %SRC_DIR%\%BPMS% %PROJECT_HOME%\target
 
 if not "%ERRORLEVEL%" == "0" (
   echo.
@@ -123,12 +97,6 @@ if not "%ERRORLEVEL%" == "0" (
 
 mkdir "%SERVER_BIN%\.niogit\"
 xcopy /Y /Q /S "%SUPPORT_DIR%\bpm-suite-demo-niogit\*" "%SERVER_BIN%\.niogit\"
-echo. 
-
-echo.
-echo - enabling demo accounts role setup in application-roles.properties file...
-echo.
-xcopy /Y /Q "%SUPPORT_DIR%\application-roles.properties" "%SERVER_CONF%"
 echo. 
 
 echo - setting up standalone.xml configuration adjustments...
